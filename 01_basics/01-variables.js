820e488d8e76ee2e3c335a5a9592ed7faa0b8cae
NHuraira <ahurairamaqsood@gmail.com>
2024-09-03 07:16:00 +0000

    variables done

---
+++ 01_basics/01-variables.js
@@ -0,0 +1,9 @@
+const rollNumber = 122
+let myName = "NHuraira"
+var myAge = 24
+myEmail = "nhuraira@gmail.com"
+let myCity;
+
+
+myName = "Huraira"
+console.table([rollNumber, myName, myAge, myEmail, myCity]);
