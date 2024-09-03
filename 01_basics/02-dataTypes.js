7adece2f14a46ca7037cbb41b4bcbfb187e1a1c3
NHuraira <ahurairamaqsood@gmail.com>
2024-09-03 07:45:47 +0000

    dataTypes done

---
+++ 01_basics/02-dataTypes.js
@@ -0,0 +1,9 @@
+let myName = "NHURAIRA"
+let myNumber = 1941471834
+let myStat = null
+let myCity;
+isLoggedIn = false
+
+// console.table([myStat, myName, myNumber, myCity, isLoggedIn])
+
+console.log(typeof undefined);
